f07d154b40acd9c70703f75482c76bc2b4ab0b4d
Mr Alamsyah <m.rifki_si10@nusaputra.ac.id>
2024-07-22 17:52:47 +0700

    bismillah bisa go lang

---
+++ Day1_pemahaman_basic.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// penggunaan variabel
+	var first_name string = "Rifki"
+
+	lastname := "Alamsyah"
+
+	fmt.Printf("halo %s %s!\n", first_name, lastname)
+}
+
+/**
+1. Penggunaan variabel di golang terdapat 2 cara bisa melalui dengan inisiasi dan deklaraasi tipe data bisa juga langsung tanpa inisiasi
+hanya saja tambahkan := untuk deklarasi nya , di golang variabel itu wajib terasign alias tidak boleh ada yang nganggur
+
+
+**/
